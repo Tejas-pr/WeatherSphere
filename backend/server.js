b4e1576b5755305f9ed400658b9c6047684ab213
Tejas-pr <Tejas.teju02@gmail.com>
2024-10-18 09:44:25 +0530

    init the project

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { weatherRoutes } = require("./routes/weatherRoutes");
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// DB connection and Server start
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  });
+
+//route
+app.use("./api/weather/", weatherRoutes);
